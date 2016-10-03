ff59e84088b439372527ff92888235ba8476d11f
Tetraa <aureliengiudici@msn.com>
2016-10-03 15:56:39 +0100

    to restart this

--- Societhy-Android/app/src/main/java/com/example/jordan/societhy_android/Constants.java
+++ Societhy-Android/app/src/main/java/com/example/jordan/societhy_android/Constants.java
@@ -6,7 +6,7 @@ import com.android.volley.RequestQueue;
  * Created by jordan on 24/02/2016.
  */
 public class Constants {
-    public static String API_URL = "http://192.168.1.15:3000/api/";
+    public static String API_URL = "http://192.168.1.143:3000/api/";
     public static String sessionToken = null;
     public static String login = null;
     public static RequestQueue queue = null;
